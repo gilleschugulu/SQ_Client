5d733127925d8882be5b7b15727935a2b21c1b61
Louis <Louis@chugulu.com>
2013-03-28 18:14:39 +0100

    sorting table on parse in progress

--- cloudCode/cloud/main.js
+++ cloudCode/cloud/main.js
@@ -1,8 +1,59 @@
-var a, b, _i, _len;
+Parse.Cloud.define("getGlobalScores", function(request, response) {
 
-a = [1, 2, 4];
+  var player;
+  var query = new Parse.Query("User");
+  query.equalTo("rank", request.params.rank);
+  query.descending("score")
+  query.find({
+    success: function(results) {
+      //Find player
+      for (var i = 0; i<results.length; i++) {
+        if(results[i].id === request.params.id)
+          player = i
+      }
+      //top postion
+      if(player<6)
+        leaderboard = addToboard(leaderboard, results, 0, 10);
+      //bottom position
+      else if(player>results.length-7)
+      {var leaderboard = [];
+        leaderboard = addToboard(leaderboard, results, 0, 3);
+        leaderboard = addToboard(leaderboard, results, results.length-7, results.length);
+      }
+      //middle position
+      else
+      {
+        leaderboard = addToboard(leaderboard, results, 0, 3);
+        leaderboard = addToboard(leaderboard, results, player-3, player+4);
+      }
+      response.success(leaderboard);
+    },
+    error: function() {
+      response.error("failed");}
+  });
+});
 
-for (_i = 0, _len = a.length; _i < _len; _i++) {
-  b = a[_i];
-  if (b > 2) console.log(b);
+function addToboard(tmp, results, min, max)
+{
+  for(var i=min; i<max; i++)
+  {
+    results[i].attributes.order = i+1;
+    tmp.push(results[i]);
+  }
+  return tmp
 }
+
+Parse.Cloud.define("sortLeaderBoard", function(request, response) {
+  var query = new Parse.Query("Test");
+  query.find({
+    success: function(results){
+      for(var i=0; i<results.length; i++)
+      {results[i].destroy();
+        console.log('object '+i);}
+      response.success('did it');
+    },
+    error: function(results){
+      response.error('didn t do it');
+    }
+  })
+});
