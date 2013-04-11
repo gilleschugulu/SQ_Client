c754d60433d3fed02774a1ab6e06079ccf59ed9b
Louis <Louis@chugulu.com>
2013-04-11 15:52:10 +0200

    preprod

--- javascripts/app.js
+++ javascripts/app.js
@@ -65874,6 +65874,7 @@ window.require.register("views/layout", function(exports, require, module) {
 });
 window.require.register("views/outgame/hall-of-fame-view", function(exports, require, module) {
   var HallOfFameView, View, template, _ref,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
@@ -65885,7 +65886,7 @@ window.require.register("views/outgame/hall-of-fame-view", function(exports, req
     __extends(HallOfFameView, _super);
 
     function HallOfFameView() {
-      _ref = HallOfFameView.__super__.constructor.apply(this, arguments);
+      this.suggestFriends = __bind(this.suggestFriends, this);    _ref = HallOfFameView.__super__.constructor.apply(this, arguments);
       return _ref;
     }
 
