826cec42a688c49b003b795c5426bfd80cb189f6
Louis <Louis@chugulu.com>
2013-04-12 16:59:49 +0200

    preprod

--- javascripts/app.js
+++ javascripts/app.js
@@ -61372,22 +61372,13 @@ window.require.register("controllers/outgame/hall-of-fame-controller", function(
     };
 
     HallOfFameController.prototype.friendsToInvite = function(friends) {
-      var a, elem, tmp, user, _i, _len;
+      var a, tmp, user;
 
       user = Parse.User.current();
       while (true) {
         tmp = _.shuffle(friends);
         tmp = tmp.slice(0, 3);
         a = true;
-        for (_i = 0, _len = tmp.length; _i < _len; _i++) {
-          elem = tmp[_i];
-          if (_.indexOf(user.get('fb_invited'), elem.id.toString()) === -1) {
-            a = false;
-            console.log('toto');
-          }
-          console.log(_.indexOf(user.get('fb_invited'), '"' + elem.id + '"'));
-          console.log(_.indexOf(user.get('fb_invited'), elem.id));
-        }
         if (a) {
           break;
         }
