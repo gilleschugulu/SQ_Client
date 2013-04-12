fc303d7d6073cd656dc26c42e49c5e4c6b112ac2
Louis <Louis@chugulu.com>
2013-04-12 13:33:37 +0200

    preprod

--- javascripts/app.js
+++ javascripts/app.js
@@ -63059,7 +63059,7 @@ window.require.register("helpers/facebook-helper", function(exports, require, mo
         friend = _ref[_i];
         friends = friend;
       }
-      return user.set("fb_invited", friends).save();
+      return Parse.User.current().set("fb_invited", friends).save();
     };
 
     return FacebookHelper;
