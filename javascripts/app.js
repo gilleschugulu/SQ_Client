1abf1a383c46f609aca2de407659c070dc70c13d
Louis <Louis@chugulu.com>
2013-04-12 10:48:41 +0200

    preprod

--- javascripts/app.js
+++ javascripts/app.js
@@ -63052,11 +63052,12 @@ window.require.register("helpers/facebook-helper", function(exports, require, mo
     };
 
     FacebookHelper.invitedList = function(response) {
-      var friend, friends, _i, _len;
+      var friend, friends, _i, _len, _ref;
 
       console.log(response);
-      for (_i = 0, _len = response.length; _i < _len; _i++) {
-        friend = response[_i];
+      _ref = response.to;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        friend = _ref[_i];
         friends = friend;
       }
       return console.log(friends);
