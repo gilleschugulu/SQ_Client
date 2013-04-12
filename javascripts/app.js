5fbf05dda1b955f9db1155c2e32a924b6af8f52f
Louis <Louis@chugulu.com>
2013-04-12 17:11:07 +0200

    preprod

--- javascripts/app.js
+++ javascripts/app.js
@@ -61383,10 +61383,9 @@ window.require.register("controllers/outgame/hall-of-fame-controller", function(
         for (_i = 0, _len = tmp.length; _i < _len; _i++) {
           elem = tmp[_i];
           if (_.indexOf(user.get('fb_invited'), elem.id) !== -1) {
-            a = true;
+            a = false;
             console.log('toto');
           }
-          console.log(_.indexOf(user.get('fb_invited'), '"' + elem.id + '"'));
           console.log(_.indexOf(user.get('fb_invited'), elem.id));
         }
         if (a) {
