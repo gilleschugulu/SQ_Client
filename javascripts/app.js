b96d20e52d8f3e783f7ee2c2355172156e216823
Louis <Louis@chugulu.com>
2013-04-22 13:06:44 +0200

    preprod

--- javascripts/app.js
+++ javascripts/app.js
@@ -63017,7 +63017,7 @@ window.require.register("helpers/facebook-helper", function(exports, require, mo
           return alert("FB.request: message doit faire entre 1 et 255 characteres (" + message.length + " actuellement)");
         }
         user = Parse.User.current();
-        console.log(user.get('fb_invited'));
+        console.log(FacebookHelper.getOtherFriends);
         return FB.ui({
           method: 'apprequests',
           message: message,
@@ -63058,7 +63058,7 @@ window.require.register("helpers/facebook-helper", function(exports, require, mo
           return alert("FB.request: message doit faire entre 1 et 255 characteres (" + message.length + " actuellement)");
         }
         user = Parse.User.current();
-        console.log(user.get('fb_invited'));
+        console.log(FacebookHelper.getOtherFriends);
         return FB.ui({
           method: 'apprequests',
           message: message,
