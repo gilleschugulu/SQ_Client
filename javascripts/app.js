995cbc9b7cad9fd84898c9cb321dafe482bf0374
Louis <Louis@chugulu.com>
2013-04-24 12:38:48 +0200

    preprod

--- javascripts/app.js
+++ javascripts/app.js
@@ -66210,7 +66210,7 @@ window.require.register("views/outgame/hall-of-fame-view", function(exports, req
       }
       console.log(Parse.User.current().get('life_given')[0]);
       console.log(player.id);
-      console.log(_.contains(Parse.User.current().get("life_given", player.id)));
+      console.log(_.indexOf(Parse.User.current().get("life_given", player.id)));
       console.log(player.id === Parse.User.current().get('life_given')[0]);
       alredySend = _.indexOf(Parse.User.current().get("life_given", player.id)) ? 'asked' : '';
       friend = player.friend ? ("<div data-id='" + player.id + "' class='ask-friend ") + alredySend + "'></div>" : '';
