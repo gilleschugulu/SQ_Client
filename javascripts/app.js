a2b687f31e3f951681ef3f3c8675d120745aed0b
Pierre Boutbel <pierre@chugulu.com>
2013-06-04 15:34:25 +0200

    preprod

--- javascripts/app.js
+++ javascripts/app.js
@@ -62687,7 +62687,7 @@ window.require.define({"controllers/outgame/home-controller": function(exports, 
 }});
 
 window.require.define({"controllers/outgame/login-controller": function(exports, require, module) {
-  var AnalyticsHelper, ConfigHelper, Controller, DeviceHelper, FacebookHelper, LequipeSSOHelper, LocalStorageHelper, LoginController, LoginView, PopUpHelper, SpinnerHelper, User, config, i18n, mediator, utils,
+  var AnalyticsHelper, ConfigHelper, Controller, DeviceHelper, FacebookHelper, LequipeSSOHelper, LocalStorageHelper, LoginController, LoginView, PopUpHelper, SpinnerHelper, User, config, i18n, mediator, utils, _,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
@@ -62721,6 +62721,8 @@ window.require.define({"controllers/outgame/login-controller": function(exports,
   LequipeSSOHelper = require('helpers/lequipe-sso-helper');
 
   ConfigHelper = require('helpers/config-helper');
+
+  _ = require('underscore');
 
   module.exports = LoginController = (function(_super) {
     var params;
